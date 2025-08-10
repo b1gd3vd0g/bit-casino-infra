3afc4c3ea442f17a24481b6ac9f32bda3489cf2b
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-10 16:54:31 -0700

    allow access instead of blocking it... lol

--- production/terraform/frontend/bucket.tf
+++ production/terraform/frontend/bucket.tf
@@ -10,10 +10,10 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_public_access_block" "bucket_access" {
   bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # Configure the S3 bucket to serve a static website. 
